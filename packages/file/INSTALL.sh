9ad920bc10cf05aa32f84abb59debdfe58ea090f
benja2998 <benja2998@duck.com>
2025-08-11 18:56:12 -0400

    Update INSTALL.sh

--- packages/file/INSTALL.sh
+++ packages/file/INSTALL.sh
@@ -4,15 +4,21 @@
 mkdir -p /usr/lib/file
 
 # Copy ./files/* to /usr/lib/file
-
 cp -f ./files/* /usr/lib/file/
 
 # Fetch kaliroot from $APPDATA/kali_in_batch/kaliroot.txt
+kaliroot=$(cat "$APPDATA/kali_in_batch/kaliroot.txt")
 
-kaliroot=$(cat $APPDATA/kali_in_batch/kaliroot.txt)
+# Define the path to add
+new_path="$USERPROFILE/kali/usr/lib/file"
 
-# Add that to .bashrc
-
-echo "export PATH=\"\$USERPROFILE/kali/usr/lib/file:\$PATH\"" >> ~/.bashrc
+# Check if new_path is already in PATH
+case ":$PATH:" in
+  *":$new_path:"*) 
+    ;;
+  *)
+    echo "export PATH=\"$new_path:\$PATH\"" >> ~/.bashrc
+    ;;
+esac
 
 echo "For the package to work, please source .bashrc to add the path to your PATH variable."
