d5f4a63c52ecaab4569c4486169f5b1e39c36f84
benja2998 <benja2998@duck.com>
2025-08-12 18:22:15 -0400

    Update neofetch.bat

--- packages/neofetch/files/neofetch.bat
+++ packages/neofetch/files/neofetch.bat
@@ -6,4 +6,4 @@ rem Fetch kaliroot from %APPDATA%\kali_in_batch\kaliroot.txt
 
 set /p kaliroot=<"%APPDATA%\kali_in_batch\kaliroot.txt" >nul 2>&1
 
-call "!kaliroot!\usr\bin\gnu-bash-wrapper.bat" "!kaliroot!\usr\lib\neofetch\neofetch"
+call "!kaliroot!\usr\bin\gnu-bash-wrapper.bat" "!kaliroot!\usr\lib\neofetch\neofetch" %*
