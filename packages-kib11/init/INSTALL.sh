e141974ceba544383c3f294a3aadd77dd8dab3ad
benja2998 <benja2998@duck.com>
2025-11-03 15:33:24 -0300

    Simplify init's INSTALl.sh
    Signed-off-by: benja2998 <benja2998@duck.com>

--- packages-kib11/init/INSTALL.sh
+++ packages-kib11/init/INSTALL.sh
@@ -5,18 +5,4 @@
 ## $ sudo kib-pkg update
 ## $ sudo kib-pkg upgrade init
 
-mkdir -p /sbin
-mkdir -p /usr/bin
-mkdir -p /usr/include
-mkdir -p /usr/lib
-mkdir -p /usr/libexec
-mkdir -p /usr/share
-mkdir -p /sbin/internal
-
-cp -f ./files/kib_in_batch.bat /sbin/init.bat
-cp -rf ./files/bin/ /usr/bin/
-cp -rf ./files/include/ /usr/include/
-cp -rf ./files/lib/ /usr/lib/
-cp -rf ./files/libexec/ /usr/libexec/
-cp -rf ./files/share/ /usr/share/
-cp -rf ./files/internal/ /sbin/internal/
+cp -rfv ./files/* /sys/kib/
