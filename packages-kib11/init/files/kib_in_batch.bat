50e01ec6269bd4a16e32e63bd5c8d47b84663c76
benja2998 <benja2998@duck.com>
2025-12-04 17:45:24 -0300

    [init] change version in all files to 11.0.2
    Signed-off-by: benja2998 <benja2998@duck.com>

--- packages-kib11/init/files/kib_in_batch.bat
+++ packages-kib11/init/files/kib_in_batch.bat
@@ -416,7 +416,7 @@ if not exist "%APPDATA%\kib_in_batch" (
 for /f "delims=" %%i in ('powershell -command "[System.Environment]::OSVersion.Version.ToString()"') do set kernelversion=%%i
 
 echo.
-echo Welcome to KIB in Batch 11.0.0-untagged ^(%PROCESSOR_ARCHITECTURE%^)
+echo Welcome to KIB in Batch 11.0.2 ^(%PROCESSOR_ARCHITECTURE%^)
 echo Booting system...
 echo ------------------------------------------------
 
@@ -606,11 +606,11 @@ echo.
 
 (
     echo NAME="KIB in Batch"
-    echo VERSION="11.0.0-untagged"
+    echo VERSION="11.0.2"
     echo ID=kibbatch
     echo ID_LIKE=linux
-    echo VERSION_ID="11.0.0-untagged"
-    echo PRETTY_NAME="KIB in Batch 11.0.0-untagged"
+    echo VERSION_ID="11.0.2"
+    echo PRETTY_NAME="KIB in Batch 11.0.2"
     echo ANSI_COLOR="0;36"
     echo HOME_URL="https://kib-in-batch.github.io"
     echo SUPPORT_URL="https://github.com/KIB-in-Batch/kib-in-batch/discussions"
@@ -641,7 +641,7 @@ if errorlevel 1 (
 )
 
 if exist "%APPDATA%\kib_in_batch\VERSION.txt" del "%APPDATA%\kib_in_batch\VERSION.txt"
-echo 11.0.0-untagged>"%APPDATA%\kib_in_batch\VERSION.txt"
+echo 11.0.2>"%APPDATA%\kib_in_batch\VERSION.txt"
 
 ::                                                                 |
 <nul set /p "=Starting Nmap service...                             "
@@ -690,7 +690,7 @@ if "%~1"=="automated" (
 
 :login
 echo.
-echo KIB in Batch 11.0.0-untagged
+echo KIB in Batch 11.0.2
 echo Kernel %kernelversion% on an %PROCESSOR_ARCHITECTURE%
 echo.
 echo Users on this system: %USERNAME%, root
