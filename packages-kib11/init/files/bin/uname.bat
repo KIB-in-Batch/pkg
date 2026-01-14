f0944ebc51a70324dcb65fb2ad3b98e5ca9c0a8a
benja2998 <benja2998@duck.com>
2026-01-14 17:27:38 -0300

    chore(init): bump version to 11.0.14
    Signed-off-by: benja2998 <benja2998@duck.com>

--- packages-kib11/init/files/bin/uname.bat
+++ packages-kib11/init/files/bin/uname.bat
@@ -105,7 +105,7 @@ goto :eof
 
 :version
 
-echo Uname for KIB in Batch 11.0.13
+echo Uname for KIB in Batch 11.0.14
 echo This is GPL-2.0-only licensed free software. There is NO WARRANTY.
 goto :eof
 
