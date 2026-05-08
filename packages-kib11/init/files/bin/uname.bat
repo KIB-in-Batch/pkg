37916528bd0ef646b03e0a45bf5831b5787547c1
benja2998 <benja2998@duck.com>
2026-05-08 14:57:12 -0400

    Fix incorrect init version

--- packages-kib11/init/files/bin/uname.bat
+++ packages-kib11/init/files/bin/uname.bat
@@ -105,7 +105,7 @@ goto :eof
 
 :version
 
-echo Uname for KIB in Batch 11.0.21
+echo Uname for KIB in Batch 11.0.23
 echo This is GPL-2.0-only licensed free software. There is NO WARRANTY.
 goto :eof
 
--- packages-kib11/init/files/kib_in_batch.bat
+++ packages-kib11/init/files/kib_in_batch.bat
@@ -201,7 +201,7 @@ if %lines% geq 100 echo.>"%ERRLOG%"
 for /f "delims=" %%i in ('powershell -command "[System.Environment]::OSVersion.Version.ToString()"') do set kernelversion=%%i
 
 echo.
-echo Welcome to KIB in Batch 11.0.21 ^(%PROCESSOR_ARCHITECTURE%^)
+echo Welcome to KIB in Batch 11.0.23 ^(%PROCESSOR_ARCHITECTURE%^)
 echo Booting system...
 echo ------------------------------------------------
 
@@ -332,11 +332,11 @@ echo.
 
 (
     echo NAME="KIB in Batch"
-    echo VERSION="11.0.21"
+    echo VERSION="11.0.23"
     echo ID=kibbatch
     echo ID_LIKE=linux
-    echo VERSION_ID="11.0.21"
-    echo PRETTY_NAME="KIB in Batch 11.0.21"
+    echo VERSION_ID="11.0.23"
+    echo PRETTY_NAME="KIB in Batch 11.0.23"
     echo ANSI_COLOR="0;36"
     echo HOME_URL="https://kib-in-batch.github.io"
     echo SUPPORT_URL="https://github.com/KIB-in-Batch/kib-in-batch/discussions"
@@ -367,7 +367,7 @@ if errorlevel 1 (
 )
 
 if exist "%APPDATA%\kib_in_batch\VERSION.txt" del "%APPDATA%\kib_in_batch\VERSION.txt"
-echo 11.0.21>"%APPDATA%\kib_in_batch\VERSION.txt"
+echo 11.0.23>"%APPDATA%\kib_in_batch\VERSION.txt"
 
 <nul set /p "=Downloading BusyBox... "
 curl -s -L -o "!kibroot!\usr\bin\busybox.exe" "%BUSYBOX_URL%"
@@ -397,7 +397,7 @@ echo %COLOR_SUCCESS%System boot completed. Boot time: !elapsed!%COLOR_RESET%
 
 :login
 echo.
-echo KIB in Batch 11.0.21
+echo KIB in Batch 11.0.23
 echo Kernel %kernelversion% on an %PROCESSOR_ARCHITECTURE%
 echo.
 set "USER=%USERNAME%"
